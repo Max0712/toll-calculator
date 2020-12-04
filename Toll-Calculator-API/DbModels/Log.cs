9e6871a6e6bd80877bc7936755a1683bddbeab7d
Louise <louise.ursing@gmail.com>
2020-12-04 10:38:52 +0100

    Added DbModels

---
+++ Toll-Calculator-API/DbModels/Log.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Toll_Calculator_API.DbModels
+{
+    public partial class Log
+    {
+        public long Id { get; set; }
+        public DateTime Created { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Toll-Calculator-API/DbModels/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+
+namespace Toll_Calculator_API.DbModels
+{
+    public partial class Vehicle
+    {
+        public Vehicle()
+        {
+            TollFees = new HashSet<TollFee>();
+        }
+
+        public long Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public long VehicleTypeId { get; set; }
+
+        public virtual VehicleType VehicleType { get; set; }
+        public virtual ICollection<TollFee> TollFees { get; set; }
+
+    }
+}
