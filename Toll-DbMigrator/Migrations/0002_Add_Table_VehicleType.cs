4f390a1f7a69acdc5d5f32cbb84568d1889d2a94
Louise <louise.ursing@gmail.com>
2020-12-04 09:08:13 +0100

    Added table and default data for VehicleType Added table for Vehicle Added table and default data for TollFee Added table for VehicleTollEvent

---
+++ Toll-DbMigrator/Migrations/0002_Add_Table_VehicleType.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+
+namespace Toll_DbMigrator.Migrations
+{
+    [Migration(0002)]
+    class _0002_Add_Table_VehicleType : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("VehicleType")
+                .WithColumn("Id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
+                .WithColumn("Type").AsString().NotNullable()
+                .WithColumn("IsFree").AsBoolean().NotNullable().WithDefaultValue(true);
+
+            Insert.IntoTable("VehicleType").Row(new { Type = "Default", IsFree = false });
+            Insert.IntoTable("VehicleType").Row(new { Type = "Military" });
+            Insert.IntoTable("VehicleType").Row(new { Type = "Motorbike" });
+            Insert.IntoTable("VehicleType").Row(new { Type = "Tractor" });
+            Insert.IntoTable("VehicleType").Row(new { Type = "Emergency" });
+            Insert.IntoTable("VehicleType").Row(new { Type = "Diplomat" });
+            Insert.IntoTable("VehicleType").Row(new { Type = "Foreign" });
+        }
+
+        public override void Down()
+        {
+            Delete.Table("VehicleType");
+        }
+    }
+}
---
+++ Toll-DbMigrator/Migrations/0005_Add_Table_VehicleTollEvent.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+
+namespace Toll_DbMigrator.Migrations
+{
+    [Migration(0005)]
+    class _0005_Add_Table_VehicleTollEvent : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("VehicleTollEvent")
+                .WithColumn("Id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
+                .WithColumn("EventTime").AsDateTime().NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime)
+                .WithColumn("VehicleId").AsGuid().NotNullable();
+
+            Create.ForeignKey()
+                .FromTable("VehicleTollEvent").ForeignColumn("VehicleId")
+                .ToTable("Vehicle").PrimaryColumn("Id");
+        }
+        public override void Down()
+        {
+            Delete.ForeignKey().FromTable("VehicleTollEvent").ForeignColumn("VehicleId")
+                       .ToTable("Vehicle").PrimaryColumn("Id");
+
+            Delete.Table("VehicleTollEvent");
+        }
+    }
+}
